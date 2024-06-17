e5eefefef91ff4c563d01f7586d7df82b2f28361
Nick Jaunich <panayao@gmail.com>
2024-06-17 14:27:33 -0400

    bugfix: Rename back to fix build error.

--- types/dto/index.ts
+++ types/dto/index.ts
@@ -1,6 +1,6 @@
 // /types/dto/index.ts
 export * from './CustomProcessingRequestDTO';
 export * from './ProductDTO';
-export * from './RFQDTO';
+export * from './rFQDTO';
 export * from './QuoteDTO';
 export * from './StockItemDTO';
